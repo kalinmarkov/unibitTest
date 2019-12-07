18e52788b5e3d2652d4ab0d6fc95aad6e8e385b7
Калин Марков <kalin_markoff@mail.bg>
2019-12-07 14:03:17 +0200

    Added MainPage

---
+++ UnibitATProject/UnibitATProject/PageObjects/MainPage.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnibitATProject.PageObjects
+{
+    public class MainPage
+    {
+        private const string Url = "https://mail.bg/auth/login";
+        private IWebDriver driver;
+
+        public MainPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void GoToMainPage()
+        {
+            driver.Navigate().GoToUrl(Url);
+        }
+    }
+}
--- UnibitATProject/UnibitATProject/TestDemo.cs
+++ UnibitATProject/UnibitATProject/TestDemo.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using UnibitATProject.PageObjects;
 
 namespace UnibitATProject
 {
@@ -10,38 +12,46 @@ namespace UnibitATProject
     public class TestDemo
     {
         private IWebDriver driver;
+        public MainPage mainPage;
 
         [TestInitialize]
         public void SetUp()
         {
             driver = new ChromeDriver();
+            mainPage = new MainPage(driver);
         }
 
         [TestCleanup]
-        protected void TearDown()
+        public void TearDown()
         {
             driver.Quit();
         }
 
         [TestMethod]
-        public void testSuccessLogin()
+        public void TestSuccessLogin()
         {
-            driver.Navigate().GoToUrl("https://mail.bg/auth/login");
+            mainPage.GoToMainPage();
+            //driver.Navigate().GoToUrl("https://mail.bg/auth/login");
             driver.Manage().Window.Size = new System.Drawing.Size(1296, 776);
             driver.FindElement(By.Id("imapuser")).SendKeys("ieee@mail.bg");
             driver.FindElement(By.Id("pass")).Click();
             driver.FindElement(By.Id("pass")).SendKeys("password");
             driver.FindElement(By.LinkText("ВЛЕЗ")).Click();
-            Assert.AreEqual(driver.FindElement(By.CssSelector("#widget_info h2")).Text, "Здравей, ieee");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            IWebElement greeting = wait.Until(d => d.FindElement(By.CssSelector("#widget_info h2")));
+            Assert.Equals(greeting.Text, "Здравей, ieee");
         }
 
         [TestMethod]
         public void TestEmptyMailAndPassword()
         {
-            driver.Navigate().GoToUrl("https://mail.bg/auth/login");
+            mainPage.GoToMainPage();
+            //driver.Navigate().GoToUrl("https://mail.bg/auth/login");
             driver.Manage().Window.Size = new System.Drawing.Size(1296, 776);
             driver.FindElement(By.LinkText("ВЛЕЗ")).Click();
-            Assert.AreEqual(driver.FindElement(By.CssSelector(".hint_text")).Text, "Попълнете правилно и двете полета");
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(15));
+            IWebElement errorMessage = wait.Until(d => d.FindElement(By.CssSelector(".hint_text")));
+            Assert.AreEqual(errorMessage.Text, "Попълнете правилно и двете полета");
             driver.Close();
         }
     }
