904d82d9859a4f5d0879c400ad998be3b9ac5e36
Калин Марков <kalin_markoff@mail.bg>
2019-12-07 13:29:04 +0200

    initial project upload

---
+++ UnibitATProject/UnibitATProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnibitATProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnibitATProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9942637e-3e41-4318-9593-0453533fb964")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnibitATProject/UnibitATProject/TestDemo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnibitATProject
+{
+    [TestClass]
+    public class TestDemo
+    {
+        private IWebDriver driver;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<string, object>();
+        }
+
+        [TestCleanup]
+        protected void TearDown()
+        {
+            driver.Quit();
+        }
+        [TestMethod]
+        public void testSuccessLogin()
+        {
+            driver.Navigate().GoToUrl("https://mail.bg/auth/login");
+            driver.Manage().Window.Size = new System.Drawing.Size(1296, 776);
+            driver.FindElement(By.Id("imapuser")).SendKeys("ieee@mail.bg");
+            driver.FindElement(By.Id("pass")).Click();
+            driver.FindElement(By.Id("pass")).SendKeys("password");
+            driver.FindElement(By.LinkText("ВЛЕЗ")).Click();
+            Assert.That(driver.FindElement(By.CssSelector("#widget_info h2")).Text, Is.EqualTo("Здравей, ieee"));
+        }
+
+        [TestMethod]
+        public void TestEmptyMailAndPassword()
+        {
+            driver.Navigate().GoToUrl("https://mail.bg/auth/login");
+            driver.Manage().Window.Size = new System.Drawing.Size(1296, 776);
+            driver.FindElement(By.LinkText("ВЛЕЗ")).Click();
+            Assert.That(driver.FindElement(By.CssSelector(".hint_text")).Text, Is.EqualTo("Попълнете правилно и двете полета"));
+            driver.Close();
+        }
+    }
+}
